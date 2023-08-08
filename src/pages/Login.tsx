d7efb1fbd43d59018ac11b6320e25297ca50b907
nandobfer <nandobfer@yahoo.com.br>
2023-08-08 19:24:56 -0300

    first commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Box} from '@mui/material'
+
+interface LoginProps {
+    
+}
+
+export const Login:React.FC<LoginProps> = ({  }) => {
+    
+    return (
+        <Box sx={{backgroundColor: 'primary.main', width: '100vw', height: '100vh'}}>
+            
+        </Box>
+    )
+}
