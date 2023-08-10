fd632d89195496bb831ba5374c061ab6b7457c16
nandobfer <nandobfer@yahoo.com.br>
2023-08-09 22:46:09 -0300

    update: home page theme

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
-import { Box, Paper } from "@mui/material"
+import { Paper } from "@mui/material"
 import { Header } from "../components/Header"
-import { useDarkMode } from "../hooks/useDarkMode"
 
 interface HomeProps {
     user: User
 }
 
 export const Home: React.FC<HomeProps> = ({ user }) => {
-    const { darkMode } = useDarkMode()
-
     return (
-        <Paper sx={{ flexDirection: "column", width: "100%", height: "100vh" }}>
+        <Paper sx={{ flexDirection: "column", width: "100%", height: "100vh", backgroundColor: "background.default" }}>
             <Header user={user} />
         </Paper>
     )
