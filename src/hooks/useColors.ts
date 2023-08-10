8c07c9898eb14e670d58f3733429e8c385a43677
nandobfer <nandobfer@yahoo.com.br>
2023-08-09 23:10:26 -0300

    fix: boz blue color

--- src/hooks/useColors.ts
+++ src/hooks/useColors.ts
@@ -4,17 +4,17 @@ export const useColors = () => {
     const { darkMode } = useDarkMode()
 
     return {
-        primary: darkMode ? "#8CDCFE" : "#6EC1E4",
+        primary: darkMode ? "#8CDCFE" : "#00AFEF",
         secondary: darkMode ? "#0078D4" : "#ffffff",
         terciary: darkMode ? "#D9D9D9" : "#1F1F1F",
 
         background: {
             primary: darkMode ? "#1F1F1F" : "#ffffff",
-            secondary: darkMode ? "#181818" : "#6EC1E4",
+            secondary: darkMode ? "#181818" : "#00AFEF",
         },
 
         text: {
-            primary: darkMode ? "#8CDCFE" : "#6EC1E4",
+            primary: darkMode ? "#8CDCFE" : "#00AFEF",
             secondary: darkMode ? "#0078D4" : "#ffffff",
             terciary: darkMode ? "#D9D9D9" : "#1F1F1F",
         },
