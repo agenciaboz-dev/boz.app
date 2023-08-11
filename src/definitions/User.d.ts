09595e5d686477fd3adb1f0f34efba9f4dcd752e
nandobfer <nandobfer@yahoo.com.br>
2023-08-11 06:37:48 -0300

    update: user and department defs

--- src/definitions/User.d.ts
+++ src/definitions/User.d.ts
@@ -4,11 +4,24 @@ declare interface User {
     email: string
     password: string
     name: string
+    cpf: string
+    birth: string
 
+    department: Department
     roles: Role[]
 }
 
 interface LoginForm {
     login: string
     password: string
+}
+
+interface UserForm {
+    username: string
+    email: string
+    name: string
+
+    cpf: string
+    birth: string
+    role: number
 }
