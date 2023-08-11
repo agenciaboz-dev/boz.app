14e568a75a3eb3fd1a56120b56f0d23d74e75268
nandobfer <nandobfer@yahoo.com.br>
2023-08-11 05:11:46 -0300

    update: tabs style

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -26,7 +26,7 @@ export const Admin: React.FC<AdminProps> = ({ user }) => {
     return (
         <Box sx={{ flexDirection: "column", width: "100vw", height: "100vh", bgcolor: "background.default" }}>
             <Header user={user} />
-            <Tabs value={tab} onChange={(_, id) => handleTabChange(id)}>
+            <Tabs value={tab} onChange={(_, id) => handleTabChange(id)} centered variant="fullWidth">
                 {tabs.map((tab) => {
                     const Icon = () => tab.icon
                     return <Tab key={tab.id} label={tab.name} value={tab.id} sx={tabStyle} icon={<Icon />} />
--- src/style/tab.ts
+++ src/style/tab.ts
@@ -1,8 +1,6 @@
 import { SxProps } from "@mui/material"
 
 export const tabStyle: SxProps = {
-    flex: 1,
     flexDirection: "row",
     gap: "1vw",
-    maxWidth: "none",
 }
