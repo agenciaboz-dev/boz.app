dfbbcdade65fe39a8b9c31e0ae676a4869d51a02
nandobfer <nandobfer@yahoo.com.br>
2023-08-11 07:22:01 -0300

    update: role container size

--- src/pages/Admin/Users/RoleContainer.tsx
+++ src/pages/Admin/Users/RoleContainer.tsx
@@ -1,21 +1,24 @@
 import React from "react"
-import { Box, MenuItem, Paper } from "@mui/material"
-import { useUser } from "../../../hooks/useUser"
+import { Badge, Box, MenuItem, Paper } from "@mui/material"
 import { Avatar } from "../../../components/Avatar"
 
-interface RoleContainerProps {}
+interface RoleContainerProps {
+    department: Department
+}
 
-export const RoleContainer: React.FC<RoleContainerProps> = ({}) => {
-    const { list } = useUser()
-
+export const RoleContainer: React.FC<RoleContainerProps> = ({ department }) => {
     return (
-        <Paper sx={{ flexDirection: "column", bgcolor: "background.default" }}>
-            {list.map((user) => (
-                <MenuItem key={user.id} sx={{ alignItems: "center", gap: "1vw" }}>
-                    <Avatar size={"3vw"} small user={user} />
-                    <p style={{ fontWeight: "bold" }}>{user.name}</p>
-                </MenuItem>
-            ))}
-        </Paper>
+        <Box sx={{ flexDirection: "column", gap: "1vw", color: "primary.main", width: "30vw" }}>
+            <p style={{ fontWeight: "bold" }}>{department.name}</p>
+
+            <Paper sx={{ flexDirection: "column", bgcolor: "background.default" }}>
+                {department.users.map((user) => (
+                    <MenuItem key={user.id} sx={{ alignItems: "center", gap: "1vw", color: "text.secondary" }}>
+                        <Avatar size={"3vw"} small user={user} />
+                        <p style={{ fontWeight: "bold" }}>{user.name}</p>
+                    </MenuItem>
+                ))}
+            </Paper>
+        </Box>
     )
 }
