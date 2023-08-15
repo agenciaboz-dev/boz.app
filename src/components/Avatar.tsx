19c58be437555476147f89b0eee9841713e6552f
nandobfer <nandobfer@yahoo.com.br>
2023-08-15 18:51:23 -0300

    feat: updating profile pic on users update

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Badge, Avatar as MuiAvatar, SxProps } from "@mui/material"
 import { useUser } from "../hooks/useUser"
 import { useImageUrl } from "../hooks/useImageUrl"
@@ -14,12 +14,17 @@ interface AvatarProps {
 
 export const Avatar: React.FC<AvatarProps> = ({ user, sx, size, small, noClickModal }) => {
     const { getProfilePic } = useImageUrl()
-    const { connectedList } = useUser()
+    const { connectedList, list } = useUser()
     const picture = usePictureModal()
     const connected = connectedList.find((item) => item.id == user.id)
-    const url = getProfilePic(user)
+
+    const [url, setUrl] = useState(getProfilePic(user))
 
     const dotSize = small ? "1vw" : "2vw"
+
+    useEffect(() => {
+        setUrl((url) => `${url}?timestamp=${new Date().getTime()}`)
+    }, [list])
 
     return (
         <Badge
