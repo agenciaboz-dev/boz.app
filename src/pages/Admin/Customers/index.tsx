189c0892821cce3107df7571160c11e3aa4bc15d
nandobfer <nandobfer@yahoo.com.br>
2023-08-15 18:41:01 -0300

    fix: padding

--- src/pages/Admin/Customers/index.tsx
+++ src/pages/Admin/Customers/index.tsx
@@ -26,7 +26,7 @@ export const Customers: React.FC<CustomersProps> = ({ user }) => {
                 width: "100%",
                 height: "100%",
                 bgcolor: "background.default",
-                padding: "3vw 4vw",
+                padding: "2vw",
                 gap: "2vw",
             }}
         >
