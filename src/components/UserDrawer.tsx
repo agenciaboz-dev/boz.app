2caf44733b49a1c6632cd11f5a0429be27eae247
tainarasn <tainarasn.sba@gmail.com>
2023-08-15 08:06:55 -0300

    feat: add username in UserDrawer

--- src/components/UserDrawer.tsx
+++ src/components/UserDrawer.tsx
@@ -4,11 +4,14 @@ import { useUser } from "../hooks/useUser"
 import { backdropStyle } from "../style/backdrop"
 import { ModeToggler } from "./ModeToggler"
 import { Avatar } from "./Avatar"
+import colors from "../style/colors"
+import { useNavigate } from "react-router-dom"
 
 interface UserDrawerProps {}
 
 export const UserDrawer: React.FC<UserDrawerProps> = ({}) => {
     const { user, drawer } = useUser()
+    const navigate = useNavigate()
 
     const handleClose = () => {
         drawer.close()
@@ -22,9 +25,20 @@ export const UserDrawer: React.FC<UserDrawerProps> = ({}) => {
             PaperProps={{ sx: { width: "22vw", backgroundColor: "background.paper" } }}
             ModalProps={{ BackdropProps: { sx: backdropStyle } }}
         >
-            <Box sx={{ padding: "2vw", flexDirection: "column", gap: "1vw", width: "100%", alignItems: "center" }} color={"secondary.main"}>
+            <Box
+                sx={{ padding: "2vw", flexDirection: "column", gap: "1vw", width: "100%", alignItems: "center" }}
+                color={"secondary.main"}
+            >
                 <Avatar user={user!} size="10vw" />
                 <p style={{ fontWeight: "bold" }}>{user?.name}</p>
+                <p
+                    style={{ fontSize: "1.0vw", color: colors.secondary, cursor: "pointer" }}
+                    onClick={() => {
+                        navigate(`admin/users/${user?.username}`)
+                    }}
+                >
+                    @{user?.username}
+                </p>
             </Box>
             <Box sx={{ flexDirection: "column" }}></Box>
 
