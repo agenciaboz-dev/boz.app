87b3f036684108b00deac6476058648bb1e33601
tainarasn <tainarasn.sba@gmail.com>
2023-08-16 12:46:09 -0300

    feix: position button

--- src/pages/Admin/Customers/index.tsx
+++ src/pages/Admin/Customers/index.tsx
@@ -39,7 +39,7 @@ export const Customers: React.FC<CustomersProps> = ({ user }) => {
                             <NewButton
                                 onClick={handleNewCustomer}
                                 bottom={"2vw"}
-                                right={"2vw"}
+                                right={"3vw"}
                                 icon={<AddIcon sx={{ width: "100%", height: "100%" }} />}
                             />
                             <Services />
