b1557b331356e290d5705497f15dd71e4c460773
nandobfer <nandobfer@yahoo.com.br>
2023-08-21 17:32:32 -0300

    update: fontsize

--- src/components/Stopwatch.tsx
+++ src/components/Stopwatch.tsx
@@ -8,7 +8,7 @@ export const Stopwatch: React.FC<StopwatchProps> = ({}) => {
     const { totalSeconds, seconds, minutes, hours, days, isRunning, start, pause, reset } = useStopwatch({ autoStart: true })
 
     return (
-        <Box sx={{ fontSize: "1vw" }}>
+        <Box sx={{ fontSize: "0.9vw" }}>
             {String(hours).padStart(2, "0")}:{String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
         </Box>
     )
