e6df4330eebefc8852e9be224398e5a52d68c884
tainarasn <tainarasn.sba@gmail.com>
2023-09-28 12:08:41 -0300

    fix: style page update

--- src/pages/Tools/Update.tsx
+++ src/pages/Tools/Update.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react"
-import { Box, Button, CircularProgress, useMediaQuery } from "@mui/material"
+import { Box, Button, CircularProgress, Paper, useMediaQuery } from "@mui/material"
 import axios from "axios"
 import CheckCircleIcon from "@mui/icons-material/CheckCircle"
 import ErrorIcon from "@mui/icons-material/Error"
 import { gitToken } from "../../api/gitToken"
 import { useUser } from "../../hooks/useUser"
+import UpdateIcon from "@mui/icons-material/Update"
+import colors from "../../style/colors"
 
 interface UpdateProps {
     user: User
@@ -39,28 +41,57 @@ export const Update: React.FC<UpdateProps> = ({ user }) => {
                 gap: isMobile ? "4vw" : "2vw",
             }}
         >
-            <h1>Atualizar Boz App</h1>
-            {electron ? (
-                <Box sx={{ flexDirection: "column", gap: "1vw" }}>
-                    <Box sx={{ gap: "1vw", alignItems: "center" }}>
-                        <p>Versão atual: {currentVersion}</p>
-                        {latestVersion == currentVersion ? <CheckCircleIcon color="success" /> : <ErrorIcon color="warning" />}
+            <h1>Atualização </h1>
+            <Paper
+                sx={{ backgroundColor: "transparent", flexDirection: "column", padding: "2vw", borderRadius: "0 3vw 0" }}
+                elevation={3}
+            >
+                {electron ? (
+                    <Box sx={{ flexDirection: "row", gap: "1vw", justifyContent: "space-between", alignItems: "center" }}>
+                        <Box sx={{ gap: "2vw" }}>
+                            <Box
+                                sx={{
+                                    backgroundColor: "primary.main",
+                                    width: "4vw",
+                                    height: "4vw",
+                                    borderRadius: "50%",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <UpdateIcon color="secondary" fontSize="large" />
+                            </Box>
+                            <Box sx={{ flexDirection: "column", justifyContent: "center" }}>
+                                <h3>Boz App </h3>
+                                <Box sx={{ gap: "1vw" }}>
+                                    <Box sx={{ gap: "0.5vw", alignItems: "center" }}>
+                                        <p style={{ fontSize: "1vw" }}>Versão atual: {currentVersion}</p>
+                                        {latestVersion == currentVersion ? (
+                                            <CheckCircleIcon color="success" />
+                                        ) : (
+                                            <ErrorIcon color="warning" />
+                                        )}
+                                    </Box>
+                                    <Box sx={{ alignItems: "center", gap: "1vw" }}>
+                                        <p style={{ fontSize: "1vw" }}>Última versão: {latestVersion}</p>
+                                        {!latestVersion && <CircularProgress size="1rem" color="primary" />}
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Box>
+                        <Button
+                            sx={{ color: "#fff", height: "3vw" }}
+                            variant="contained"
+                            disabled={!!latestVersion && latestVersion == currentVersion}
+                            onClick={() => window.open(downloadUrl, "_blank")?.focus()}
+                        >
+                            Baixar atualização
+                        </Button>
                     </Box>
-                    <Box sx={{ alignItems: "center", gap: "1vw" }}>
-                        <p>Última versão: {latestVersion}</p>
-                        {!latestVersion && <CircularProgress size="1rem" color="primary" />}
-                    </Box>
-                    <Button
-                        variant="contained"
-                        disabled={!!latestVersion && latestVersion == currentVersion}
-                        onClick={() => window.open(downloadUrl, "_blank")?.focus()}
-                    >
-                        Baixar atualização
-                    </Button>
-                </Box>
-            ) : (
-                <Box>Você não tá usando o app, se saia</Box>
-            )}
+                ) : (
+                    <Box>Você não tá usando o app, se saia</Box>
+                )}
+            </Paper>
         </Box>
     )
 }
