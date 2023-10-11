c68ec8d374c81695ce2b844cf749088239dab96e
nandobfer <nandobfer@yahoo.com.br>
2023-10-11 16:06:04 -0300

    feat: added desktop notification

--- src/contexts/warningsContext.tsx
+++ src/contexts/warningsContext.tsx
@@ -1,6 +1,9 @@
 import { createContext, useEffect, useState } from "react"
 import React from "react"
 import { useIo } from "../hooks/useIo"
+import { url } from "../api/backend"
+import { useConfirmDialog } from "burgos-confirm"
+import { useNavigate } from "react-router-dom"
 
 interface WarningsContextValue {
     list: Warning[]
@@ -16,13 +19,39 @@ const WarningsContext = createContext<WarningsContextValue>({} as WarningsContex
 export default WarningsContext
 
 export const WarningsProvider: React.FC<WarningsProviderProps> = ({ children }) => {
+    const iconUrl = `http${url}/static/favicon.ico`
     const io = useIo()
+    const navigate = useNavigate()
+
+    const { confirm } = useConfirmDialog()
 
     const [list, setList] = useState<Warning[]>([])
 
     useEffect(() => {
         io.on("warning:new", (warning) => {
             setList((list) => [...list, warning])
+
+            const notification = new Notification("Aviso!", {
+                body: "Existe um novo aviso, vai lá ver",
+                icon: iconUrl,
+            })
+
+            notification.onclick = (ev) => {
+                window.focus()
+                if (window.parent) {
+                    window.parent.focus()
+                }
+            }
+
+            confirm({
+                title: "Novo aviso",
+                content: "Existe um novo aviso, vai lá ver",
+                hideCancel: true,
+                button: "Claro",
+                onConfirm: () => {
+                    navigate("/warnings")
+                },
+            })
         })
 
         io.on("warning:update", (warning) => {
