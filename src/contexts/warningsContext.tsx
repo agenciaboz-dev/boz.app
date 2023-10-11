9b5dc407ce1ae97896f2fcadfa4d2e864c5ffef9
nandobfer <nandobfer@yahoo.com.br>
2023-10-11 16:13:01 -0300

    update: not notifying creator

--- src/contexts/warningsContext.tsx
+++ src/contexts/warningsContext.tsx
@@ -4,6 +4,7 @@ import { useIo } from "../hooks/useIo"
 import { url } from "../api/backend"
 import { useConfirmDialog } from "burgos-confirm"
 import { useNavigate } from "react-router-dom"
+import { useUser } from "../hooks/useUser"
 
 interface WarningsContextValue {
     list: Warning[]
@@ -22,36 +23,40 @@ export const WarningsProvider: React.FC<WarningsProviderProps> = ({ children }) 
     const iconUrl = `http${url}/static/favicon.ico`
     const io = useIo()
     const navigate = useNavigate()
+    
+    const { user } = useUser()
 
     const { confirm } = useConfirmDialog()
 
     const [list, setList] = useState<Warning[]>([])
 
     useEffect(() => {
-        io.on("warning:new", (warning) => {
+        io.on("warning:new", (warning: Warning) => {
             setList((list) => [...list, warning])
 
-            const notification = new Notification("Aviso!", {
-                body: "Existe um novo aviso, vai lá ver",
-                icon: iconUrl,
-            })
+            if (warning.creator.id != user?.id) {
+                const notification = new Notification("Aviso!", {
+                    body: "Existe um novo aviso, vai lá ver",
+                    icon: iconUrl,
+                })
 
-            notification.onclick = (ev) => {
-                window.focus()
-                if (window.parent) {
-                    window.parent.focus()
+                notification.onclick = (ev) => {
+                    window.focus()
+                    if (window.parent) {
+                        window.parent.focus()
+                    }
                 }
-            }
 
-            confirm({
-                title: "Novo aviso",
-                content: "Existe um novo aviso, vai lá ver",
-                hideCancel: true,
-                button: "Claro",
-                onConfirm: () => {
-                    navigate("/warnings")
-                },
-            })
+                confirm({
+                    title: "Novo aviso",
+                    content: "Existe um novo aviso, vai lá ver",
+                    hideCancel: true,
+                    button: "Claro",
+                    onConfirm: () => {
+                        navigate("/warnings")
+                    },
+                })
+            }
         })
 
         io.on("warning:update", (warning) => {
