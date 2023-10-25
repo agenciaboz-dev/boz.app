6b924d9d1844d3aed9d95b57ab87234355eac57b
nandobfer <nandobfer@yahoo.com.br>
2023-10-25 10:51:05 -0300

    update: wakeup only for devs

--- src/pages/Tools/Wakeup/index.tsx
+++ src/pages/Tools/Wakeup/index.tsx
@@ -10,6 +10,7 @@ import { ApiPage } from "./ApiPage"
 import { backgroundStyle } from "../../../style/background"
 import ApiIcon from "@mui/icons-material/Api"
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline"
+import { useUser } from "../../../hooks/useUser"
 interface WakeupProps {
     user: User
 }
@@ -33,6 +34,7 @@ export const Wakeup: React.FC<WakeupProps> = ({ user }) => {
     const { list } = useWakeup()
     const colors = useColors()
     const navigate = useNavigate()
+    const { isRole } = useUser()
 
     const [electron] = useState(window.electron)
 
@@ -60,46 +62,53 @@ export const Wakeup: React.FC<WakeupProps> = ({ user }) => {
                         }}
                     >
                         {electron ? (
-                            <>
-                                <Paper
-                                    sx={{
-                                        width: isMobile ? "100%" : "17vw",
-                                        height: isMobile ? "30vh" : "100%",
-                                        bgcolor: "background.paper",
-                                        flexDirection: "column",
-                                        overflowY: "auto",
-                                        padding: "1vw 3vw",
-                                        alignItems: "center",
-                                        gap: isMobile ? "5vw" : "1.5vw",
-                                        borderRadius: isMobile ? "0 2vw 0 0" : "0 0 0 2vw ",
-                                        color: "secondary.main",
-                                    }}
-                                    elevation={4}
-                                >
-                                    <Title title="APIs">
-                                        <Box sx={{ padding: "0 0.5vw", justifyContent: "flex-end" }}>
-                                            <Tooltip title="New API" dir="top">
-                                                <IconButton sx={{ justifyContent: "flex-end" }} onClick={() => navigate("/tools/wakeup/new")}>
-                                                    <Add color="secondary" />
-                                                </IconButton>
-                                            </Tooltip>
-                                        </Box>
-                                        {list
-                                            .sort((a, b) => a.id - b.id)
-                                            .map((api) => (
-                                                <ApiContainer api={api} key={api.id} />
-                                            ))}
-                                    </Title>
-                                </Paper>
+                            isRole("dev") ? (
+                                <>
+                                    <Paper
+                                        sx={{
+                                            width: isMobile ? "100%" : "17vw",
+                                            height: isMobile ? "30vh" : "100%",
+                                            bgcolor: "background.paper",
+                                            flexDirection: "column",
+                                            overflowY: "auto",
+                                            padding: "1vw 3vw",
+                                            alignItems: "center",
+                                            gap: isMobile ? "5vw" : "1.5vw",
+                                            borderRadius: isMobile ? "0 2vw 0 0" : "0 0 0 2vw ",
+                                            color: "secondary.main",
+                                        }}
+                                        elevation={4}
+                                    >
+                                        <Title title="APIs">
+                                            <Box sx={{ padding: "0 0.5vw", justifyContent: "flex-end" }}>
+                                                <Tooltip title="New API" dir="top">
+                                                    <IconButton sx={{ justifyContent: "flex-end" }} onClick={() => navigate("/tools/wakeup/new")}>
+                                                        <Add color="secondary" />
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </Box>
+                                            {list
+                                                .sort((a, b) => a.id - b.id)
+                                                .map((api) => (
+                                                    <ApiContainer api={api} key={api.id} />
+                                                ))}
+                                        </Title>
+                                    </Paper>
 
-                                <Box sx={{ width: isMobile ? "100%" : "80vw" }}>
-                                    <Routes>
-                                        <Route index element={<></>} />
-                                        <Route path="/new" element={<NewApi user={user} />} />
-                                        <Route path="/api/:id/*" element={<ApiPage user={user} />} />
-                                    </Routes>
+                                    <Box sx={{ width: isMobile ? "100%" : "80vw" }}>
+                                        <Routes>
+                                            <Route index element={<></>} />
+                                            <Route path="/new" element={<NewApi user={user} />} />
+                                            <Route path="/api/:id/*" element={<ApiPage user={user} />} />
+                                        </Routes>
+                                    </Box>
+                                </>
+                            ) : (
+                                <Box sx={{ gap: "0.5vw", alignItems: "center" }}>
+                                    <ErrorOutlineIcon color="primary" />
+                                    <p>Essa ferramenta só está disponível para desenvolvedores.</p>
                                 </Box>
-                            </>
+                            )
                         ) : (
                             <Box sx={{ gap: "0.5vw", alignItems: "center" }}>
                                 <ErrorOutlineIcon color="primary" />
