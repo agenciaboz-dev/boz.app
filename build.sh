a1c79c17b4107b6b486c0319f50385a716d7caca
nandobfer <nandobfer@yahoo.com.br>
2023-11-16 19:27:04 -0300

    update: script for new server

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-user="agenciaboz"
-subdomain="app.agenciaboz.com.br"
+user="agencyboz.com"
+subdomain="app.agencyboz.com"
 path="/home/${user}/${subdomain}"
 
 npx vite build
 echo 'Uploading build to server'
-scp -r -P 22022 dist/* agenciaboz:${path}
-ssh -p 22022 agenciaboz "chown -R ${user}:${user} ${path}/*"
+scp -r dist/* root@agencyboz:${path}
+# ssh -p 22022 agenciaboz "chown -R ${user}:${user} ${path}/*"
