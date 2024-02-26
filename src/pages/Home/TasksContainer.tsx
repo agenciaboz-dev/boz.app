46a2d281440b1baf41e26894aae55b14ccc1dc37
nandobfer <nandobfer@yahoo.com.br>
2024-02-26 16:54:49 -0300

    feat: added weather home

--- src/pages/Home/TasksContainer.tsx
+++ src/pages/Home/TasksContainer.tsx
@@ -1,13 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Box } from "@mui/material"
 import { ContainerWrapper } from "./ContainerWrapper"
+import { ContainerSkeleton } from "./ContainerSkeleton"
+import axios from "axios"
 
 interface TasksContainerProps {}
 
 export const TasksContainer: React.FC<TasksContainerProps> = ({}) => {
-    return (
+    const [weather, setWeather] = useState("")
+
+    const getWeather = async () => {
+        const response = await axios.get("https://wttr.in/curitiba?lang=pt-br&format=1")
+        setWeather(response.data.replace("+", " "))
+    }
+
+    useEffect(() => {
+        getWeather()
+    }, [])
+
+    return weather ? (
         <ContainerWrapper>
-            <Box sx={{ fontSize: "1.2rem", fontWeight: "bold" }}>Tarefinhas aparacerão aqui</Box>
+            <Box sx={{ fontSize: "5rem", fontWeight: "bold", justifyContent: "center", alignItems: "center", height: 1 }}>{weather}</Box>
         </ContainerWrapper>
+    ) : (
+        <ContainerSkeleton />
     )
 }
