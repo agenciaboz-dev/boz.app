3445065ccc15577307b09676bfe7d4ed57ff9d71
tainarasn <tainarasn.sba@gmail.com>
2024-04-16 12:30:08 -0300

    fix: weather component

---
+++ src/definitions/weather.d.ts
@@ -0,0 +1,31 @@
+declare interface CurrentConditions {
+    datetime: string
+    datetimeEpoch: number
+    temp: number
+    feelslike: number
+    humidity: number
+    dew: number
+    precip: number
+    precipprob: number
+    snow: number
+    snowdepth: number
+    preciptype: null | string
+    windgust: null | number
+    windspeed: number
+    winddir: number
+    pressure: number
+    visibility: number
+    cloudcover: number
+    solarradiation: number
+    solarenergy: number
+    uvindex: number
+    conditions: string
+    icon: string
+    stations: string[]
+    source: string
+    sunrise: string
+    sunriseEpoch: number
+    sunset: string
+    sunsetEpoch: number
+    moonphase: number
+}
