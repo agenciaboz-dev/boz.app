6a36e3e575cc1c0707f6be986ab32a047191bd41
nandobfer <nandobfer@yahoo.com.br>
2024-05-09 20:49:18 -0300

    fix: index batch?

--- src/pages/Tools/Nagazap/Oven/Batch.tsx
+++ src/pages/Tools/Nagazap/Oven/Batch.tsx
@@ -17,7 +17,7 @@ export const Batch: React.FC<BatchProps> = ({ batch, nagazap, index }) => {
                 name={`${index + 1}`}
                 right={
                     <Box sx={{ fontSize: "0.9rem" }}>
-                        {new Date(Number(nagazap.lastMessageTime) + (Number(nagazap.frequency) * index + 1)).toLocaleString("pt-br")}
+                        {new Date(Number(nagazap.lastMessageTime) + Number(nagazap.frequency) * (index + 1)).toLocaleString("pt-br")}
                     </Box>
                 }
             />
